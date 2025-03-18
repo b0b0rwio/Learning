44d44f53d49484cd3428390ae408e14f745949fc
b0b0rwio <bagavathi7828@gmail.com>
2025-03-18 14:00:32 +0530

    comments added

--- JavaScript-Learning/js/callStack.js
+++ JavaScript-Learning/js/callStack.js
@@ -5,3 +5,11 @@ function square(num){
 }
 var num1=square(2)
 console.log(num1)
+
+// GEC - n:2, Square:{...}, num:Square(2) [in memory phase]
+//EC-1 - inside the GEC - (num:undefinded, ans:undefined in memeory phase)
+// CallStack-
+//           |   |  
+//           |EC1|
+//           |GEC| -> popped after all execution phases is done
+//           |___|
